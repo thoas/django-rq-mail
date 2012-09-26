b92d684b1a3d65938923fc2887f4937241bf8c27
Florent Messa <florent.messa@gmail.com>
2012-09-26 11:26:14 +0200

    Add README to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
+import os
 from setuptools import setup, find_packages
 
 version = __import__('rq_mail').__version__
+
+root = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(root, 'README.rst')) as f:
+    README = f.read()
 
 setup(
     name='django-rq-mail',
     version=version,
     description='simple Python library based on rq to store emails sent by Django and process them in the background with workers',
+    long_description=README,
     author='Florent Messa',
     author_email='florent.messa@gmail.com',
     url='http://github.com/thoas/django-rq-mail',
